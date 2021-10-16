474e351c745342280b07b055894581f44e3d5a39
Asmenys <76791232+Asmenys@users.noreply.github.com>
2021-10-17 00:44:17 +0300

    Added an insert and delete methods for Tree class, delete only works with leaf nodes for now.

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -3,7 +3,7 @@
 require 'pry-byebug'
 
 class Node
-  attr_reader :data, :left, :right
+  attr_accessor :data, :left, :right
 
   def initialize(data = nil, left = nil, right = nil)
     @data = data
@@ -24,7 +24,7 @@ class Tree
     if array.empty?
       return nil
     else
-      mid_element = array.delete_at((array.length-1)/2)
+      mid_element = array.delete_at((array.length - 1) / 2)
       left_sub_array = array[0...array.length / 2].to_a
       right_sub_array = array[array.length / 2...array.length].to_a
       node = Node.new(mid_element, build_tree(left_sub_array),
@@ -34,6 +34,40 @@ class Tree
     node
   end
 
+  def insert(value, node = @root)
+    if node.right.nil? && node.left.nil?
+      if node.data > value
+        node.left = Node.new(value)
+      else
+        node.right = Node.new(value)
+      end
+    else
+      next_node = if node.data > value
+                    node.left
+                  else
+                    node.right
+                  end
+      insert(value, next_node)
+    end
+  end
+
+  def delete(value, node = @root)
+    if node.right.data == value
+      node.right = nil if node.right.right.nil? && node.right.left.nil?
+    elsif node.left.data == value
+      if node.left.right.nil? && node.left.left.nil?
+        node.left = nil
+      end
+    else
+      next_node = if node.data > value
+                    node.left
+                  else
+                    node.right
+                  end
+      delete(value, next_node)
+    end
+  end
+
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
@@ -41,7 +75,7 @@ class Tree
   end
 end
 i = 0
-array = Array.new(20) {i+=1}
+array = Array.new(9) { i += 1 }
 tree = Tree.new(array)
 binding.pry
 bin = 'bin'
